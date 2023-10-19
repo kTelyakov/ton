6494ce2f2fd45ef5cd2fa25e04736e40d06bb5c3
konstantin <telyakov.k.v@gmail.com>
2023-10-19 22:51:51 +0300

    feat: lint

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
-import { mnemonicToWalletKey } from 'ton-crypto'
-import {fromNano, internal, TonClient, WalletContractV4} from "ton";
-import {getHttpEndpoint} from "@orbs-network/ton-access";
+import { mnemonicToWalletKey } from "ton-crypto"
+import { fromNano, internal, TonClient, WalletContractV4} from "ton"
+import { getHttpEndpoint } from "@orbs-network/ton-access"
 
 async function main () {
-  const mnemonik = 'cry crazy debris shaft history husband obscure found crouch divide boss garlic access reward feature obey wait rural enforce giggle hollow exit ocean jealous'
-  const key = await mnemonicToWalletKey(mnemonik.split(' '))
+  const mnemonik = "cry crazy debris shaft history husband obscure found crouch divide boss garlic access reward feature obey wait rural enforce giggle hollow exit ocean jealous"
+  const key = await mnemonicToWalletKey(mnemonik.split(" "))
 
   const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 })
   const endpoint = await getHttpEndpoint({ network: "testnet" })
   const client = new TonClient({ endpoint })
 
   if (!await client.isContractDeployed(wallet.address)) {
-    console.log('Contract not deployed')
+    console.log("Contract not deployed")
   }
 
-  console.log('Walled is deployed : ', wallet.address)
+  console.log("Walled is deployed : ", wallet.address)
 
   const balance = await client.getBalance(wallet.address)
 
-  console.log('balance : ', fromNano(balance))
+  console.log("balance : ", fromNano(balance))
 
   // EQBn8cBUDMRvyKXteceaA5RWRItVL3g7t-RzfbqA71ABAdCR
 
@@ -30,9 +30,9 @@ async function main () {
     seqno: segno,
     messages: [
       internal({
-        to: 'EQA4V9tF4lY2S_J-sEQR7aUj9IwW-Ou2vJQlCn--2DLOLR5e',
-        value: '0.05',
-        body: 'hello',
+        to: "EQA4V9tF4lY2S_J-sEQR7aUj9IwW-Ou2vJQlCn--2DLOLR5e",
+        value: "0.05",
+        body: "hello",
         bounce: false,
       })
     ]
@@ -40,11 +40,11 @@ async function main () {
 
   let currentSeqno = segno
   while (currentSeqno==segno) {
-    console.log('waiting for transaction to confirm...')
+    console.log("waiting for transaction to confirm...")
     await sleep(1500)
     currentSeqno = await walletContract.getSeqno()
   }
-  console.log('transaction confirmed')
+  console.log("transaction confirmed")
 }
 
 main()
